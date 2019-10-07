34f2d799f99ae98bd1f6f31fcb7ce542710ca553
Tim Nagel <tim@nagel.com.au>
2019-10-08 10:19:50 +1100

    OpenApiTypeResolver

---
+++ src/Generator/Interfaces/OpenApiTypeResolverInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\ApiDocumenter\Generator\Interfaces;
+
+use Symfony\Component\PropertyInfo\Type;
+
+interface OpenApiTypeResolverInterface
+{
+    /**
+     * Returns the OpenAPI type to be used for the property type.
+     *
+     * @param \Symfony\Component\PropertyInfo\Type $type
+     *
+     * @return mixed[]
+     */
+    public function resolvePropertyType(Type $type): array;
+}
